2a803062f5c8d0b32859da0a64c374ab34e1c2c5
liuyi <1722032245@qq.com>
2017-09-20 21:35:17 +0800

    io字节读取

--- src/main/java/assemblage/single.java
+++ src/main/java/assemblage/single.java
@@ -1,15 +1,22 @@
 package assemblage;
 
-import java.util.List;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2017\9\12 0012.
  * List
  * Collection
  * Iterable
+ * ArrayList中remove注意可以使用Integer传参，删除的是值，使用int传参删除的是索引对应的值。
  */
 public class single {
     public static void main(String[] args) {
-
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(2);
+        arrayList.add(1);
+        arrayList.remove(1);//注意的是int是基本对象，底层使用是否是对象进行判断。
+        arrayList.remove(new Integer(1));//为包装类型，是对象。
+        System.out.println(arrayList);
     }
 }
